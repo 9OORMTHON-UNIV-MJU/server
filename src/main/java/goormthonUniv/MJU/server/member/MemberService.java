ab08f9e4ed34128f811c96abfcc86ad61f4eb8c0
wonbj <kaiuo97@naver.com>
2025-06-20 15:26:10 +0900

    Feat: 회원가입 기능 구현

--- src/main/java/goormthonUniv/MJU/server/member/MemberService.java
+++ src/main/java/goormthonUniv/MJU/server/member/MemberService.java
@@ -1,10 +1,12 @@
 package goormthonUniv.MJU.server.member;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Service;
 
 import goormthonUniv.MJU.server.global.config.JwtTokenProvider;
 import goormthonUniv.MJU.server.global.exception.CustomLoginException;
+import goormthonUniv.MJU.server.global.exception.CustomRegisterException;
 import goormthonUniv.MJU.server.global.exception.ExceptionCode;
 import goormthonUniv.MJU.server.member.dto.LoginRequest;
 import goormthonUniv.MJU.server.member.dto.LoginResponse;
@@ -21,6 +23,29 @@ public class MemberService {
     private final JwtTokenProvider jwtTokenProvider;
     private final PasswordEncoder passwordEncoder;
 
+    // 회원가입 로직
+ 	public void register(RegisterRequest request) {
+ 		
+ 		// 닉네임 중복 확인
+ 		if (memberRepository.findByNickname(request.getNickname()).isPresent()) {
+ 			throw new CustomRegisterException(ExceptionCode.DUPLICATE_NICKNAME);
+ 		}
+ 		
+ 		// 비밀번호 암호화
+ 		String encodedPassword = passwordEncoder.encode(request.getPassword());
+ 		
+ 		// Member 엔티티 생성 및 저장
+         Member newMember = Member.builder()
+                 .nickname(request.getNickname())
+                 .password(encodedPassword)
+                 .role(request.getRole())
+                 .belonging(request.getBelonging())
+                 .build();
+
+         memberRepository.save(newMember);
+ 		
+ 	}
+    
     // 로그인 로직
 	public LoginResponse login(LoginRequest request) {
 		
